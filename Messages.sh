4b0fe8eaced4fe0032bf6fe73dd986a786821832
Nate Graham <nate@kde.org>
2022-09-16 15:56:55 -0600

    Internally call everything "plasma-welcome"
    This is more consistent and also fixes the window icon

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 # SPDX-FileCopyrightText: None
 # SPDX-License-Identifier: CC0-1.0
 
-$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o $podir/welcomecenter.pot
+$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o $podir/plasma-welcome.pot
