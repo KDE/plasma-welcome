f217153867f009c65167fd1aff5a64dcb6b2c8b5
Nate Graham <nate@kde.org>
2022-09-21 10:23:50 -0600

    Don't set hidpi attributes on Qt6; they're automatic there

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,10 @@ int main(int argc, char *argv[])
     format.setOption(QSurfaceFormat::ResetNotification);
     QSurfaceFormat::setDefaultFormat(format);
 
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
