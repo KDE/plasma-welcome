2b88642e58219db8942e606940c4b59662372d05
Laurent Montel <montel@kde.org>
2023-07-05 19:48:43 +0200

    Add missing #pragma once

--- src/kded/daemon.h
+++ src/kded/daemon.h
@@ -4,6 +4,8 @@
  *  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
  */
 
+#pragma once
+
 #include <KConfigGroup>
 #include <QVersionNumber>
 #include <kdedmodule.h>
