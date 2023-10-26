ea8513942e9297ff6f83624a6a57411082f1d1a0
Laurent Montel <montel@kde.org>
2023-10-26 06:53:47 +0200

    GIT_SILENT: use KLocalizedString::setApplicationDomain(QByteArrayLiteral => saving one deep data copy

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
     QCoreApplication::setApplicationName(QStringLiteral("plasma-welcome"));
-    KLocalizedString::setApplicationDomain("plasma-welcome");
+    KLocalizedString::setApplicationDomain(QByteArrayLiteral("plasma-welcome"));
 
     const QString description = i18nc("@info:usagetip", "A welcome app for KDE Plasma");
     KAboutData aboutData(
