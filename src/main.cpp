90f7b4163d28d2ec9e1a37703ec183ae5f383f22
Oliver Beard <olib141@outlook.com>
2024-06-09 11:37:22 +0100

    main: Update copyright statement Updates copyright range to extend to 2024 and attributes "Plasma contributors".

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
         // The license this code is released under.
         KAboutLicense::GPL,
         // Copyright Statement.
-        i18nc("@info copyright string", "(c) 2021"));
+        i18nc("@info copyright string", "© 2021-2024, KDE Community"));
     aboutData.setProgramLogo(QIcon::fromTheme(QStringLiteral("start-here-kde-plasma")));
     aboutData.addAuthor(i18nc("@info:credit", "Felipe Kinoshita"),
                         i18nc("@info:credit", "Author"),
