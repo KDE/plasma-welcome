0b4ab2203640fe685fcd1a1f4903b778b2e06391
Oliver Beard <olib141@outlook.com>
2025-09-06 10:19:28 +0100

    applicationInfo: Refactor such that we can update when KSycoca's database is changed
    By updating when KSycoca's database is changed, we can respond to changes such as an application that was previously uninstalled being installed, rather than serving the same static information.
    
    There is no API change that should break external pages, just that our application properties are now properly read-only and can update later.

--- src/public/applicationInfo.cpp
+++ src/public/applicationInfo.cpp
@@ -1,5 +1,5 @@
 /*
- *  SPDX-FileCopyrightText: 2023 Oliver Beard <olib141@outlook.com>
+ *  SPDX-FileCopyrightText: 2025 Oliver Beard <olib141@outlook.com>
  *
  *  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
  */
@@ -9,35 +9,68 @@
 #include <QString>
 
 #include <KService>
+#include <KSycoca>
+
+ApplicationInfo::ApplicationInfo(QObject *parent)
+    : QObject(parent)
+{
+    // Update application information when KSycoca's database is changed,
+    // e.g. it may have been installed later
+    connect(KSycoca::self(), &KSycoca::databaseChanged, this, &ApplicationInfo::update);
+}
+
+ApplicationInfo::~ApplicationInfo()
+{
+}
+
+QString ApplicationInfo::desktopName() const
+{
+    return m_desktopName;
+}
 
 void ApplicationInfo::setDesktopName(const QString &desktopName)
 {
-    if (m_desktopName == desktopName) {
-        return;
+    if (m_desktopName != desktopName) {
+        m_desktopName = desktopName;
+        Q_EMIT desktopNameChanged();
+
+        update();
     }
+}
 
-    m_desktopName = desktopName;
-    Q_EMIT desktopNameChanged();
+bool ApplicationInfo::exists() const
+{
+    return m_exists;
+}
 
+QString ApplicationInfo::name() const
+{
+    return m_name;
+}
+
+QString ApplicationInfo::icon() const
+{
+    return m_icon;
+}
+
+void ApplicationInfo::update()
+{
     const KService::Ptr service = KService::serviceByDesktopName(m_desktopName);
 
-    const bool exists = (service != nullptr);
+    bool exists = (service != nullptr);
+    QString name = service ? service->name() : QString();
+    QString icon = service ? service->icon() : QString();
+
     if (m_exists != exists) {
         m_exists = exists;
         Q_EMIT existsChanged();
     }
 
-    if (!exists) {
-        return;
-    }
-
-    const QString name = service->name();
     if (m_name != name) {
         m_name = name;
         Q_EMIT nameChanged();
     }
 
-    const QString icon = service->icon();
     if (m_icon != icon) {
         m_icon = icon;
         Q_EMIT iconChanged();
