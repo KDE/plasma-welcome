9c156b125c27cbca815a92edaba0aa7d03bfce0a
Laurent Montel <montel@kde.org>
2025-09-21 15:36:13 +0200

    GIT_SILENT: Don't duplicate headers between .h/.cpp

--- src/release.cpp
+++ src/release.cpp
@@ -6,8 +6,6 @@
  *  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
  */
 
-#include <QNetworkInformation>
-#include <QNetworkReply>
 #include <QRegularExpression>
 #include <QTextDocumentFragment>
 
